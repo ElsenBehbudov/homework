79c3556938366b1f0e6e8ad15d09a3cd2de25e1a
ElsenBehbudov <hakanoztun@gmail.com>
2025-09-15 15:49:23 +0400

    asdfg-final

---
+++ jss_learn/main.js
@@ -0,0 +1,14 @@
+function stringlenght(metn=""){
+    return metn.lenght;
+}
+console.log(stringlenght("salam"));
+
+function upperstring(up=""){
+    return up.upper();
+}
+console.log (upperstring("salam"));
+
+function smallstring(smal=""){
+    return up.upper();
+}
+console.log (upperstring("salam"));
